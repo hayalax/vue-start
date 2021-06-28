9f4e224ea94f76dd89f72e8457586d5aca3cb856
hayalax <86593868+hayalax@users.noreply.github.com>
2021-06-28 13:32:09 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    hours: 0,
+    scm: 50,
+   
   },
   mutations: {
+    SET_HOURS(state, hours1) {
+      state.hours = hours1.target.value
+    },
+    SET_scm(state, scm) {
+      state.scm = scm.target.value
+    }, 
+    SET_zero(state) {
+      state.scm = 50
+      state.hours = 0
+      
+    },
   },
   actions: {
+  /*  async getTodos({ commit }) {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/todos/')
+      console.log(res);
+      commit('SET_TODOS', res.data)
+    }*/
+  },
+  getters: {
+  
+    sum_to_hour(state) {
+      return state.scm*state.hours
+    },
   },
   modules: {
   }
